78a4246de2d734713f17d3883cfd53801ac9e754
uvaprol <120366489+uvaprol@users.noreply.github.com>
2024-03-14 16:35:21 +0300

    update return button

--- scripts/buton_up_scroll.js
+++ scripts/buton_up_scroll.js
@@ -1,9 +1,12 @@
 const btn = document.getElementById('up')
+let timerId = setTimeout(()=>{btn.style.display = 'none'}, 500)
 window.onwheel = (e) =>{
     if (e.deltaY < 0){
-        btn.style.display = 'block'
         btn.style.animation = 'visible 1.5s forwards'
+        clearTimeout(timerId)
+        btn.style.display = 'block'
     } else{
         btn.style.animation = 'unvisible 1.5s forwards'
+        timerId = setTimeout(()=>{btn.style.display = 'none'}, 500)
     }
 }
